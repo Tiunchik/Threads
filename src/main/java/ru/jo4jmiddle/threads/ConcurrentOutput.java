77cf53f331e8e504f122fc1ad2b6f7c7ae151f53
Tiunchik <Senebh@gmail.com>
2020-03-08 00:15:46 +0300

    1. Запуск нити. Thread#start()[#249939] [#00006]

---
+++ src/main/java/ru/jo4jmiddle/threads/ConcurrentOutput.java
@@ -0,0 +1,35 @@
+/**
+ * Package ru.jo4jmiddle.threads for
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.jo4jmiddle.threads;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Class ConcurrentOutput - class for first steps into multithreading.
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 08.03.2020
+ */
+public class ConcurrentOutput {
+
+    /**
+     * Main.
+     *
+     * @param args - args
+     */
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        second.start();
+        another.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
