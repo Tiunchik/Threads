f2aed6dd83cae5e4efbb4a8cfa9068d9fb890e81
Tiunchik <Senebh@gmail.com>
2020-03-09 11:16:27 +0300

    1.2. Режим ожидания.[#249938] [#00013]

---
+++ src/main/java/ru/jo4jmiddle/threads/Loader.java
@@ -0,0 +1,42 @@
+/**
+ * Package ru.jo4jmiddle.threads for
+ *
+ * @author Maksim Tiunchik
+ */
+package ru.jo4jmiddle.threads;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Class Loader - class for work with Thread.sleep method
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 09.03.2020
+ */
+public class Loader {
+
+    /**
+     * main.
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        Thread loader = new Thread(() -> {
+            System.out.println("Start loading:");
+            for (int persent = 0; persent <= 100; persent++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.print(String.format("\rloaded %5d persentages", persent));
+            }
+            System.out.print("\rLoading is finihsed");
+        });
+        loader.start();
+    }
+
+
+}
