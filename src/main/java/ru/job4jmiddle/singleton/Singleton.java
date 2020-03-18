ae47b65e617d073001917faebc91a5ac5fed12d1
Tiunchik <Senebh@gmail.com>
2020-03-18 20:17:14 +0300

    0. ThreadSafe Singleton[#249928] [#00044]

---
+++ src/main/java/ru/job4jmiddle/singleton/Singleton.java
@@ -0,0 +1,34 @@
+package ru.job4jmiddle.singleton;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Class Singleton example
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 18.03.2020
+ */
+public class Singleton {
+
+    private static volatile AtomicReference<String> INSTANCE;
+
+    private Singleton() {
+    }
+
+    public static AtomicReference<String> getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new AtomicReference<String>();
+        }
+        return INSTANCE;
+    }
+
+    public String add(String model) {
+        return model;
+    }
+
+    public static void main(String[] args) {
+        AtomicReference<String> tracker = Singleton.getInstance();
+    }
+}
+
